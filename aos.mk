11afa509cd7c16c539e8ee77b74ae7fac4872dc8
blackarashi <36367761+blackarashi@users.noreply.github.com>
2018-04-25 18:12:36 +0200

    Atomic-OS ify

--- aos.mk
+++ aos.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, device/xiaomi/tissot/full_tissot.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+$(call inherit-product, vendor/aos/config/common_full_phone.mk)
 
 # A/B updater
 AB_OTA_UPDATER := true
@@ -52,12 +52,15 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     libgptutils \
     libz
 
-PRODUCT_NAME := rr_tissot
+PRODUCT_NAME := aos_tissot
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
-    
+
 BUILD_FINGERPRINT := xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys
+
+# Unofficial build ID
+-TARGET_UNOFFICIAL_BUILD_ID := DELUXETEAM
